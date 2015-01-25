4ae5060da760e5f8f02ad2f6ec2361ab9883653b
Benjamin Conant <conantbenjamin@gmail.com>
2015-01-25 02:18:35 -0500

    implemented multiple email send

--- client/app/card/card.controller.js
+++ client/app/card/card.controller.js
@@ -6,7 +6,7 @@ angular.module('fuzziesApp')
     var self = this;
     $scope.showSpinner = false;
     $scope.card = {
-    	message: "Hey There!\n\nWelcome to Fuzzies! Send one to someone special! ... It's sure to brighten up their day :) \n\n Love,\n\nThe Fuzz Doctor",
+    	message: "Hey There!\n\nWelcome to Fuzzies! Send one to someone special ... It's sure to brighten up their day :) \n\n Love,\n\nDr. FuzzyMcMailer",
     	senderEmail: "",
       email: "", 
       backgroundColor: "",
